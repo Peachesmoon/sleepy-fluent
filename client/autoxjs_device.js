d7446a79d6679c158c12cbb6e3620db6e85ffb29
Peachesmoon <132327956+Peachesmoon@users.noreply.github.com>
2025-02-01 09:49:07 +0800

    Update autoxjs_device.js

--- client/autoxjs_device.js
+++ client/autoxjs_device.js
@@ -7,9 +7,9 @@ Co-authored-by: NyaOH-Nahida - 新增捕捉退出事件，将退出脚本状态�
 
 // config start
 const API_URL = 'https://sleepy.moonpeaches.com:9010/device/set'; // 你的完整 API 地址，以 `/device/set` 结尾
-const SECRET = '绝对猜不出来的密钥'; // 你的 secret
-const ID = '设备ID'; // 你的设备 id
-const SHOW_NAME = '设备名称'; // 你的设备名称
+const SECRET = '621126'; // 你的 secret
+const ID = 'device-2'; // 你的设备 id
+const SHOW_NAME = '桃子的垃圾手机'; // 你的设备名称
 const CHECK_INTERVAL = '2000'; // 检查间隔 (毫秒)
 // config end
 
